fa43d612e3f31d4752a292dc98b6a58ae0e29d40
Ben Daniels <benbanj@hotmail.com>
2020-06-28 18:22:21 +0100

    Add Hirb to console load

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module FlightBooker
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    console do 
+      Hirb.enable
+    end
   end
 end
