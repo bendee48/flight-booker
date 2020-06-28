9a65597ecd4332cfaac7aa0f68bdc09d59ec6a5e
Ben Daniels <benbanj@hotmail.com>
2020-06-28 22:02:08 +0100

    Adding Search query methods

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,8 +2,12 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all
     @dates = Flight.all.map do |flight| 
-      date = flight.start_time.strftime("%m/%d/%Y")
-      [date, date]
-    end.uniq.sort
+      date = flight.start_time.strftime("%d/%m/%Y")
+      [date, flight.start_time]
+    end.uniq { |date| date.first }.sort.reverse
+
+    selected_date = params[:date]
+    @search_results = Flight.where('start_time = ?', selected_date)
+    p selected_date
   end
 end
