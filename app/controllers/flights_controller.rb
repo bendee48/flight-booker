8425e526265c44169b0769bd25674585f1c2c35e
Ben Daniels <benbanj@hotmail.com>
2020-06-30 12:36:36 +0100

    Search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -6,8 +6,17 @@ class FlightsController < ApplicationController
       [date, flight.start_time]
     end.uniq { |date| date.first }.sort.reverse
 
-    selected_date = params[:date]
-    @search_results = Flight.where('start_time = ?', selected_date)
-    p selected_date
+    if params[:from_id]
+    @airport = Airport.find(params[:from_id])
+    end
+
+    p @airport
+
+    # if params[:date]
+    #   # @selected_date = params[:date].scan(/\d{4}-\d{2}-\d{2}/)
+    #   # p @selected_date
+    #   # @search_results = Flight.where(start_time: ("#{selected_date} 00:01:01 UTC".."#{selected_date} 2020-07-03 23:59:59"))
+    #   # p @search_results
+    # end
   end
 end
