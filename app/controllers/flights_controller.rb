b80851e75b4414e699a91dc2cfa58d5fd2969629
Ben Daniels <benbanj@hotmail.com>
2020-06-30 17:26:02 +0100

    Refactor search methods

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,13 +2,6 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all
     @dates = Flight.format_dates_for_selection
-
-    if params[:date]
-      selected_date = params[:date].scan(/\d{4}-\d{2}-\d{2}/)
-      to_airport = params[:to_id]
-      from_airport = params[:from_id]
-      @search_results = Flight.includes([:from_airport, :to_airport]).where(start_time: ("#{selected_date} 00:00:01 UTC".."#{selected_date} 23:59:59"), start_airport_id: from_airport, finish_airport_id: to_airport)
-    end
-
+    @search_results = Flight.search(params[:search]) if params[:search]
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -8,4 +8,11 @@ class Flight < ApplicationRecord
       [date, flight.start_time]
     end.uniq { |date| date.first }.sort.reverse
   end
+
+  def self.search(search_params)
+    selected_date = search_params[:date].scan(/\d{4}-\d{2}-\d{2}/)
+    to_airport = search_params[:to_id]
+    from_airport = search_params[:from_id]
+    includes([:from_airport, :to_airport]).where(start_time: ("#{selected_date} 00:00:01 UTC".."#{selected_date} 23:59:59"), start_airport_id: from_airport, finish_airport_id: to_airport)
+  end
 end
