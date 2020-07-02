ba6dfd15985fd1ebf3a5038166d2b4474f9d2d1b
Ben Daniels <benbanj@hotmail.com>
2020-07-02 14:33:08 +0100

    Reworking booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,8 +2,30 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
     @flight = Flight.find(params[:flight])
+    @booking.flight = @flight
+    passenger_num = params[:passengers].to_i
+    passenger_num.times { @booking.passengers.build }
   end
 
   def create
+    params[:booking][:flight_id] = params[:flight][:id]
+    @booking = Booking.new(booking_params)
+    
+    if @booking.save
+      flash.notice = "hurrah"
+      redirect_to @booking
+    else
+      flash.notice = "Something went wrong!"
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
 end
