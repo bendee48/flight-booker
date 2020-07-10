a54ab2d34480d0cfc026ff6a274827711af7cfe7
Ben Daniels <benbanj@hotmail.com>
2020-07-10 18:24:07 +0100

    Adding email send on booking confirmation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,7 +13,7 @@ class BookingsController < ApplicationController
 
     if @booking.save
       flash.notice = "hurrah"
-      PassengerMailer.with(booking: @booking).booking_confirmed.deliver_now
+      PassengerMailer.booking_confirmed(@booking)
       redirect_to @booking
     else
       flash.now.notice = "Aargh"
--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,8 +1,13 @@
 class PassengerMailer < ApplicationMailer
-  def booking_confirmed
-    booking = params[:booking]
-    p params
-    @passenger_email = booking.passengers.first.email
-    mail(to: @passenger_email, subject: 'Thanks for booking with us!' )
+  def self.booking_confirmed(booking)
+    passenger_emails = booking.passengers.map { |p| p.email }
+    
+    passenger_emails.each do |email|
+      send_booking_confirmation(email).deliver_later
+    end
+  end
+
+  def send_booking_confirmation(email)
+    mail(to: email, subject: 'Thanks for booking with us!')
   end
 end
